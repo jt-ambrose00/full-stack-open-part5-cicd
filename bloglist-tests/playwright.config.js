654ed7a41cc78b68196c368ec6276a5e31d5cd98
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-10-26 15:47:46 -0500

    attempting to fix playwright.config.js webServer configuration

--- bloglist-tests/playwright.config.js
+++ bloglist-tests/playwright.config.js
@@ -72,8 +72,8 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:8080',
+    command: 'cd ../bloglist-frontend && npm install && npm run start',
+    url: 'http://localhost:5173',
     timeout: 30 * 1000,
     reuseExistingServer: !process.env.CI,
   },
