5295935fcb42a87cbc17a46e50abae15d37619d8
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-10-28 15:42:49 -0500

    added automatic deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 app.get('/version', (req, res) => {
-    res.send('1') // change this string to ensure a new version deployed
+    res.send('2') // change this string to ensure a new version deployed
 })
 
 app.listen(config.PORT, () => {
